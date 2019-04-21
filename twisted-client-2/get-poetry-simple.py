eb01d61fc499dd89166eca0d94bcd6549ea5aa69
brentweiliu <brentweiliu@oath.com>
2019-04-20 18:09:59 -0700

    Part 5: Twistier Poetry Transport Protocal ProtocalFactory

--- twisted-client-2/get-poetry-simple.py
+++ twisted-client-2/get-poetry-simple.py
@@ -9,59 +9,33 @@ from twisted.internet.protocol import Protocol, ClientFactory
 
 def parse_args():
     usage = """usage: %prog [options] [hostname]:port ...
-
 This is the Get Poetry Now! client, Twisted version 2.1.
 Run it like this:
-
-  python get-poetry-simple.py port1 port2 port3 ...
-
-If you are in the base directory of the twisted-intro package,
-you could run it like this:
-
   python twisted-client-2/get-poetry-simple.py 10001 10002 10003
-
-to grab poetry from servers on ports 10001, 10002, and 10003.
-
-Of course, there need to be servers listening on those ports
-for that to work.
 """
-
     parser = optparse.OptionParser(usage)
-
     _, addresses = parser.parse_args()
-
     if not addresses:
         print parser.format_help()
         parser.exit()
-
     def parse_address(addr):
         if ':' not in addr:
             host = '127.0.0.1'
             port = addr
         else:
             host, port = addr.split(':', 1)
-
         if not port.isdigit():
             parser.error('Ports must be integers.')
-
         return host, int(port)
-
     return map(parse_address, addresses)
 
-
 class PoetryProtocol(Protocol):
-
     poem = ''
-
     def dataReceived(self, data):
         self.poem += data
 
     def connectionLost(self, reason):
-        self.poemReceived(self.poem)
-
-    def poemReceived(self, poem):
-        self.factory.poem_finished(poem)
-
+        self.factory.poem_finished(self.poem)
 
 class PoetryClientFactory(ClientFactory):
 
@@ -74,7 +48,6 @@ class PoetryClientFactory(ClientFactory):
     def poem_finished(self, poem=None):
         if poem is not None:
             self.poems.append(poem)
-
         self.poetry_count -= 1
 
         if self.poetry_count == 0:
@@ -93,17 +66,12 @@ class PoetryClientFactory(ClientFactory):
 
 def poetry_main():
     addresses = parse_args()
-
     factory = PoetryClientFactory(len(addresses))
-
     from twisted.internet import reactor
-
     for address in addresses:
         host, port = address
         reactor.connectTCP(host, port, factory)
-
     reactor.run()
-
 
 if __name__ == '__main__':
     poetry_main()
--- twisted-client-2/get-poetry.py
+++ twisted-client-2/get-poetry.py
@@ -9,48 +9,28 @@ from twisted.internet.protocol import Protocol, ClientFactory
 
 def parse_args():
     usage = """usage: %prog [options] [hostname]:port ...
-
 This is the Get Poetry Now! client, Twisted version 2.0.
 Run it like this:
-
-  python get-poetry.py port1 port2 port3 ...
-
-If you are in the base directory of the twisted-intro package,
-you could run it like this:
-
   python twisted-client-2/get-poetry.py 10001 10002 10003
-
-to grab poetry from servers on ports 10001, 10002, and 10003.
-
-Of course, there need to be servers listening on those ports
-for that to work.
 """
 
     parser = optparse.OptionParser(usage)
-
     _, addresses = parser.parse_args()
-
     if not addresses:
         print parser.format_help()
         parser.exit()
-
     def parse_address(addr):
         if ':' not in addr:
             host = '127.0.0.1'
             port = addr
         else:
             host, port = addr.split(':', 1)
-
         if not port.isdigit():
             parser.error('Ports must be integers.')
-
         return host, int(port)
-
     return map(parse_address, addresses)
 
-
 class PoetryProtocol(Protocol):
-
     poem = ''
     task_num = 0
 
@@ -60,17 +40,13 @@ class PoetryProtocol(Protocol):
         print  msg % (self.task_num, len(data), self.transport.getPeer())
 
     def connectionLost(self, reason):
-        self.poemReceived(self.poem)
+        self.factory.poem_finished(self.task_num, self.poem)
 
-    def poemReceived(self, poem):
-        self.factory.poem_finished(self.task_num, poem)
 
 
 class PoetryClientFactory(ClientFactory):
-
     task_num = 1
-
-    protocol = PoetryProtocol # tell base class what proto to build
+    protocol = PoetryProtocol  # tell base class what proto to build
 
     def __init__(self, poetry_count):
         self.poetry_count = poetry_count
@@ -101,26 +77,19 @@ class PoetryClientFactory(ClientFactory):
         print 'Failed to connect to:', connector.getDestination()
         self.poem_finished()
 
-
 def poetry_main():
     addresses = parse_args()
-
     start = datetime.datetime.now()
 
     factory = PoetryClientFactory(len(addresses))
-
     from twisted.internet import reactor
-
     for address in addresses:
         host, port = address
         reactor.connectTCP(host, port, factory)
-
     reactor.run()
 
     elapsed = datetime.datetime.now() - start
-
     print 'Got %d poems in %s' % (len(addresses), elapsed)
-
 
 if __name__ == '__main__':
     poetry_main()
